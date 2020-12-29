868b1c74e2f4e711565e65906e0f34052fd5800c
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 11:18:49 +0000

    i

--- installwww.sh
+++ installwww.sh
@@ -19,7 +19,6 @@ apt-get install xvfb
 dpkg --configure -a
   /etc/init.d/x11-common start
 export DISPLAY=:0
-#nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
-nohup Xvfb :0 -listen tcp -screen 0 1024x768x24 &
+nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
 sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost + 
 #& xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh   
