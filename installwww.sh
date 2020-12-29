3fd8cdf1f74705b65464fef78268d22b47b83dd1
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 07:31:34 +0000

    k

--- installwww.sh
+++ installwww.sh
@@ -9,15 +9,16 @@
   apt-get install xdotool
   apt-get install xterm
   apt-get install xvkbd
-  apt-get install mwm
+#  apt-get install mwm
+apt-get install xfwm4
  apt-get install x11-apps
  apt-get install x11-xserver-tools
 #apt-get install fvwm-crystal
   apt-get install firefox-esr
-apt-get install kde-spectacle
+#apt-get install kde-spectacle
 dpkg --configure -a
   /etc/init.d/x11-common start
   export DISPLAY=:0
 #nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
 nohup Xvfb :0 -listen tcp -screen 0 1024x768 &
-bash --norc --noprofile -c ' sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost + & xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh  '
+bash --norc --noprofile -c ' sleep 2; xfwm4 & firefox-esr & xterm -rv & xterm -rv & xhost + & xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh  '
--- kali.sh
+++ kali.sh
@@ -1,4 +1,5 @@
 export DISPLAY=:0
-yes |apt-get install x11-xserver-tools
+yes |apt-get install x11-xserver-utils
+yes |apt-get install net-tools
 xhost +
-  docker run -i -t -p 127.0.0.1:6080:6080/tcp -p 127.0.0.1:6000:6000/tcp -p 127.0.0.1:8080:8080/tcp -p 127.0.0.1:2222:22/tcp --expose 6000 --privileged   kalilinux/kali bash -c "apt-get update; apt-get upgrade; yes |apt-get install xterm;yes|apt-get install git;yes|apt-get install x11-xserver-tools;yes|apt-get install openssh-server; yes "" | adduser mark; perl -e 'print "x\n";print "x\n";'|passwd mark; /etc/init.d/ssh start; export DISPLAY=:0; cd; git clone https://github.com/distortedmirror/httpX.git; cd httpX; ./STARTHTTPX.sh & /bin/bash -l"
+  docker run -i -t -p 127.0.0.1:6080:6080/tcp -p 127.0.0.1:6000:6000/tcp -p 127.0.0.1:8080:8080/tcp -p 127.0.0.1:2222:22/tcp --expose 6000 --privileged   kalilinux/kali bash -c "apt-get update; apt-get upgrade; yes |apt-get install xterm;yes|apt-get install git;yes|apt-get install x11-xserver-utils;yes|apt-get install nettools;yes|apt-get install vim;yes|appt-gete install emacs;yes|apt-get install openssh-server; yes "" | adduser mark; perl -e 'print "x\n";print "x\n";'|passwd mark; /etc/init.d/ssh start; export DISPLAY=:0; cd; git clone https://github.com/distortedmirror/httpX.git; cd httpX; ./STARTHTTPX.sh & /bin/bash -l"
