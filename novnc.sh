378b93ce449e5cac89fcd6cac92cb10ee84e03a5
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 17:36:01 +0000

    ok

--- novnc.sh
+++ novnc.sh
@@ -1,9 +1,10 @@
 killall x11vnc
 killall websockify
 killall python
-yes | apt-get install x11vnc
+#yes | apt-get install x11vnc
 yes | apt-get install novnc
+yes |apt-get install xtightvncviewer
 export DISPLAY=:0
-x11vnc --loop --forever &
+#x11vnc --loop --forever &
 sleep 4
 /usr/share/novnc/utils/launch.sh
