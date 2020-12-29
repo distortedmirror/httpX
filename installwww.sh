c50c07442b00f0cedbc840f97986fbbe4d35af3e
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 06:53:16 +0000

    nohup

--- installwww.sh
+++ installwww.sh
@@ -13,24 +13,13 @@
   sudo apt-get install xvkbd
   sudo apt-get install mwm
  sudo apt-get install x11-apps
+ sudo apt-get install x11-xserver-tools
 #sudo apt-get install fvwm-crystal
   sudo apt-get install firefox-esr
 sudo apt-get install kde-spectacle
 sudo dpkg --configure -a
   sudo /etc/init.d/x11-common start
   export DISPLAY=:0
-Xvfb :0 -listen tcp -screen 0 800x600x24&
-  sleep 2
-  mwm &
-  #fvwm2 &
-  #xfce4-session &
-  #fvwm-crystal &
-  sleep 2
-  xeyes &
-  firefox-esr &
-  xterm -rv &
-  xterm -rv &
-  sleep 2
-  xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg
-  ./www.sh &
-./novnc.sh  
+#sudo nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
+sudo nohup Xvfb :0 -listen tcp -screen 0 1024x768 &
+sudo bash --norc --noprofile -c ' sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost + & xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh  '
