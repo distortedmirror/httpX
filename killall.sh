1c68896e8b55742cd7edd1cbf89306629dd1b725
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 21:18:56 +0000

    o

---
+++ killall.sh
@@ -0,0 +1,10 @@
+sudo killall Xvfb
+sudo killall firefox-esr
+sudo killall xtightvnc
+sudo killall xterm
+sudo killall websockify
+sudo killall Xvfb
+sudo killall firefox-esr
+sudo killall xtightvnc
+sudo killall xterm
+sudo killall websockify
