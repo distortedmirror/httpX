b3bba643474ac0c6a9e629a95140338b68fadeb3
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 16:31:55 +0000

    ok

--- installwww.sh
+++ installwww.sh
@@ -13,6 +13,7 @@ apt-get install xvfb
  apt-get install x11-xserver-tools
   apt-get install firefox-esr
   apt-get install mwm
+  apt-get install cinnamon
   apt-get install x11vnc
   apt-get install novnc
 #apt-get install kde-spectacle
@@ -20,5 +21,4 @@ dpkg --configure -a
   /etc/init.d/x11-common start
 export DISPLAY=:0
 nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
-sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost + 
-#& xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh   
+sleep 2; cinnamon & firefox-esr & xterm -rv & xterm -rv & xhost + & xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh   
