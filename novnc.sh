5d7167baef990fcb766fa728fb6756badcaffdad
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-29 17:53:49 +0000

    ok

--- novnc.sh
+++ novnc.sh
@@ -4,7 +4,9 @@ killall python
 #yes | apt-get install x11vnc
 yes | apt-get install novnc
 yes |apt-get install xtightvncviewer
+yes |apt-get install xauth
+yes |apt-get install x11vnc
 export DISPLAY=:0
 #x11vnc --loop --forever &
-sleep 4
+#sleep 4
 /usr/share/novnc/utils/launch.sh
