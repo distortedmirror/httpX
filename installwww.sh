98a9d8884a18f32a471d24d6164de9d4f5f154c1
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-03 06:05:31 +0000

    OK

--- installwww.sh
+++ installwww.sh
@@ -1,4 +1,4 @@
-#yes | apt-get update
+yes | apt-get update
 #yes | apt-get upgrade
 apt-get install lsof
   apt-get install nmap
@@ -9,17 +9,18 @@ apt-get install xvfb
   apt-get install xdotool
   apt-get install xterm
   apt-get install xvkbd
- apt-get install x11-apps
+# apt-get install x11-apps
  apt-get install x11-xserver-tools
   apt-get install firefox-esr
   apt-get install mwm
   apt-get install x11vnc
   #apt-get install novnc
-apt-get install kde-spectacle
-dpkg --configure -a
+#apt-get install kde-spectacle
+#dpkg --configure -a
 /etc/init.d/x11-common start
 export DISPLAY=:0
-nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
+#nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
+nohup Xvfb :0 -listen tcp -screen 0 1024x768x8 &
 sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost +  
-x11vnc -rfbport 5901 --loop --forever 
+x11vnc -display :0 -rfbport 5901 --loop --forever 
 #xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh   
