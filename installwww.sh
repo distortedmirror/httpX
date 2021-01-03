467a383c542d850baedfcf51440d4849035ca2bb
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-03 07:25:33 +0000

    k

--- installwww.sh
+++ installwww.sh
@@ -2,13 +2,13 @@ yes | apt-get update
 #yes | apt-get upgrade
 apt-get install lsof
   apt-get install nmap
-apt-get install imagemagick
+#apt-get install imagemagick
   apt-get install ncat
   apt-get install netcat
 apt-get install xvfb
   apt-get install xdotool
   apt-get install xterm
-  apt-get install xvkbd
+#  apt-get install xvkbd
 # apt-get install x11-apps
  apt-get install x11-xserver-tools
   apt-get install firefox-esr
@@ -19,8 +19,8 @@ apt-get install xvfb
 #dpkg --configure -a
 /etc/init.d/x11-common start
 export DISPLAY=:0
+./novnc.sh
 #nohup Xvfb :0 -listen tcp -screen 0 800x600x24 &
-nohup Xvfb :0 -listen tcp -screen 0 1024x768x8 &
-sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost +  
-x11vnc -display :0 -rfbport 5901 --loop --forever 
+#sleep 2; mwm & firefox-esr & xterm -rv & xterm -rv & xhost +  
+#x11vnc -display :0 -rfbport 5901 --loop --forever 
 #xwd -root -display :0 -out screen.xwd ; convert screen.xwd screen.jpg ; ./www.sh & ./novnc.sh   
