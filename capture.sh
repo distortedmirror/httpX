b6e914450bda58aab3ce06db2e290ec8fa0a9814
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-03 14:21:04 +0000

    ok

--- capture.sh
+++ capture.sh
@@ -1,1 +1,1 @@
-xwd -root -display :1 -out screen.xwd ; convert -quality 48 -strip screen.xwd screen.jpg
+xwd -root -display :0 -out screen.xwd ; convert -quality 48 -strip screen.xwd screen.jpg
--- response.sh
+++ response.sh
@@ -1,6 +1,5 @@
 #/bin/bash
-export DISPLAY=:1
-read DISPLAY
+export DISPLAY=:0
 killall xdotool
  urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 read input
