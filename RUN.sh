30629133b8712d13df23e4cd8ea3a41f822023a3
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-03 08:34:18 +0000

    ok

--- RUN.sh
+++ RUN.sh
@@ -1,4 +1,5 @@
 echo Run ./novncdockerkali.sh on cloud to start container
 echo Run ./kalidockerattach.sh to attach to container
 echo Wait until loaded
-echo Then run ./novnc.sh on cloud to open nvnc
+echo Then run ./novncdockerkali.sh on cloud to open novnc
+echo (If fails try ./killall.sh;./novncdockerkali.sh on cloud and ./killall.sh;./novnc.sh on container
