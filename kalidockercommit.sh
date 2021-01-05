8ca045de9a76bb6602a32478630ef0b19595be82
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-05 06:54:23 +0000

    iok

--- kalidockercommit.sh
+++ kalidockercommit.sh
@@ -1,1 +1,2 @@
-docker commit `cat kalicontainerid.txt` markbrito1975/kali:version2
+export containerid=`cat containerid.txt`
+docker commit `docker container inspect $containerid |grep Id|perl -pe "s/^.*d0fc/d0fc/g;s/\",.*//g;"` markbrito1975/kali:version3
