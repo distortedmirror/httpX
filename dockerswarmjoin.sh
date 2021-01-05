59f44cf044db1063d7ff6946a3fc04a03f9b37f0
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-05 10:36:15 +0000

    ikali

---
+++ dockerswarmjoin.sh
@@ -0,0 +1,1 @@
+docker swarm join --token SWMTKN-1-3rt1oafksy6fghrq426p8byozh8tvr03agdwmprkv80spyvhhu-59ywuxq0umqthnkfqk6uye4qd 172.17.0.4:2377
--- gcloudauth.sh
+++ gcloudauth.sh
@@ -1,1 +1,1 @@
-gcloud auth login 2>&1|tee out;echo Now Download out.html; mv out out.html
+gcloud auth configure-docker
