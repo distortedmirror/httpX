1c61ee372b3bcaf391befe7514ecd1d3a1405c2f
Mark Brito <mark.brito.gcloud@gmail.com>
2021-01-18 12:21:10 +0000

    ok//

---
+++ 2-novnc-browser.sh
@@ -0,0 +1,2 @@
+yes | apt-get install novnc
+/usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 8888
---
+++ N.sh
@@ -0,0 +1,1 @@
+ docker container exec d4024920206d bash -c "cd;cd  httpX;./killall.sh;export DISPLAY=:0;Xvfb :1;" & sudo ./novncdockerkali.sh 
